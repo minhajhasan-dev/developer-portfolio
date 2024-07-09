e759e6b14d51a845dd1a105e635d944b464cb43e
minhaj.dev <minhajhasan.dev@gmail.com>
2024-07-09 20:33:58 +0600

    added vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes/Routes.jsx";
 import "./utility/custom.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlowCapture>
       <RouterProvider router={router} />
       <Toaster />
+      <Analytics />
+      <SpeedInsights/>
     </GlowCapture>
   </React.StrictMode>
 );
